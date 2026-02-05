87ea94b4175ec70e4d41967bd57bfdd1faefffaa
Shashika2001 <shashika.ekanayaka@aahaas.com>
2026-02-05 14:24:58 +0530

    create the controller

---
+++ src/main/java/com/example/docker_kubernetes/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.docker_kubernetes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    StudentRepo repo;
+
+    @PostMapping
+    public Student save(@RequestBody Student s) {
+        return repo.save(s);
+    }
+
+    @GetMapping
+    public List<Student> all() {
+        return repo.findAll();
+    }
+}
